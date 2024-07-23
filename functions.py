58a8f150900068b8e35b9493dc1c570a17594363
LeireCoboC <Leire.Cobo@cognizant.com>
2024-07-23 11:02:39 +0000

    update summary changes

--- functions.py
+++ functions.py
@@ -45,66 +45,6 @@ def read_Img(image_path):
     return full_text
 
 
-def summarize_pdf(path): 
-    '''It summarizes the content found in the image given.'''
-    
-    # Extract the text from the given file
-    full_text = read_pdf(path)
-       
-    # Use a chatbot with an API key
-    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzk2Y2QzNzgtZDhlZS00MzkxLWJmYmMtNzg1N2I0NWJjMDYyIiwidHlwZSI6ImFwaV90b2tlbiJ9.BIwMWdaPNAcVM6yhNbLMD2SDbmTxfis_Emd6HUYfVME"}
-
-    url = "https://api.edenai.run/v2/text/chat"
-    payload = {
-        "providers": "openai",
-        "text": full_text,
-        "chatbot_global_action": f"""Actua como un asistente cuya funcion es ayudar a abogados
-                                 y personal legal a resumir documentos jurídicos extensos,
-                                 destacando los datos más importantes para ahorrando tiempo 
-                                 y esfuerzo en el análisis manual.
-                                 
-                                 Para la salida quiero que lo ordenes por puntos importantes, con una idea por punto y pongas Resumen: """,
-        "previous_history": [],
-        "temperature": 0.0,
-        "max_tokens": 1000,
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-
-    result = json.loads(response.text)
-    return result['openai']['generated_text']
-
-
-def summarize_Img(path): 
-    '''It summarizes the content found in the image given.'''
-    
-    # Extract the text from the given image
-    full_text = read_Img(path)
-       
-    # Use a chatbot with an API key
-    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzk2Y2QzNzgtZDhlZS00MzkxLWJmYmMtNzg1N2I0NWJjMDYyIiwidHlwZSI6ImFwaV90b2tlbiJ9.BIwMWdaPNAcVM6yhNbLMD2SDbmTxfis_Emd6HUYfVME"}
-
-    url = "https://api.edenai.run/v2/text/chat"
-    payload = {
-        "providers": "openai",
-        "text": full_text,
-        "chatbot_global_action": f"""Actua como un asistente cuya funcion es ayudar a abogados
-                                 y personal legal a resumir documentos jurídicos extensos,
-                                 destacando los datos más importantes para ahorrando tiempo 
-                                 y esfuerzo en el análisis manual.
-                                 
-                                 Para la salida quiero que lo ordenes por puntos importantes, con una idea por punto y pongas Resumen: """,
-        "previous_history": [],
-        "temperature": 0.0,
-        "max_tokens": 1000,
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-
-    result = json.loads(response.text)
-    return result['openai']['generated_text']
-
-#####################################################################################
 def summarize_pdf_eng(path): 
     '''It summarizes the content found in the image given.'''
     
@@ -119,7 +59,7 @@ def summarize_pdf_eng(path):
         "providers": "openai",
         "text": full_text,
         "chatbot_global_action": f"""Act as a paralegal who has to summarize different models of wills from different countries.
-                                  You have to return this structure:
+                                  You have to return a summary in a single paragraph using connectors and including all these chacteristics:
                                  
                                    - Identification of the testator: full name, date of birth, nationality and address.
                                    - Date of execution: Day, month and year in which the will is drawn up.
@@ -160,7 +100,7 @@ def summarize_Img_eng(path):
         "providers": "openai",
         "text": full_text,
         "chatbot_global_action": f"""Act as a paralegal who has to summarize different models of wills from different countries.
-                                  You have to return this structure:
+                                  You have to return a summary in a single paragraph using connectors and including all these chacteristics:
                                  
                                    - Identification of the testator: full name, date of birth, nationality and address.
                                    - Date of execution: Day, month and year in which the will is drawn up.
